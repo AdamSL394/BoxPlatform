9245ce6de758572e411d25a2c2b3b5bda82492f2
Adam <lehreradam@yahoo.com>
2019-10-03 13:06:52 -0700

    adding functions to upload file and create a user

--- credentials.js
+++ credentials.js
@@ -1,6 +1,6 @@
 const boxSDK = require('box-node-sdk');
 const fs = require('fs');
-const express = require ("express")
+const express = require("express")
 const PORT = process.env.PORT || 3000
 const app = express()
 // const routes = require("./routes")
@@ -20,14 +20,42 @@ app.use(express.json());
 app.use(routes)
 
 function getAllClient() {
-    client.users.get(client.CURRENT_USER_ID)
-      .then(user => {
-        console.log('Hello', user.name, '!')
-        // .catch(err => console.log('Got an error!', err));
-      })
-  }
+  client.users.get(client.CURRENT_USER_ID)
+    .then(user => {
+      console.log('Hello', user.name, '!')
+      // .catch(err => console.log('Got an error!', err));
+    })
+}
 
+
+function uploadFilePromise() {
+
+  var stream = fs.createReadStream('/Users/adam/Desktop/Desktop/CoverLetter.docx');
+  var folderID = "0";
+  client.files.uploadFile(folderID, 'Name1', stream)
+    .then(file => console.log('Hello', file, '!'))
+    .catch(err => console.log('Got an error mate!', err));
+  console.log(client)
+}
+
+function createAppUser() {
+  // Set app user details
+  const userName = 'Adam Lehrer1';
+  const spaceAmount = 1073741824;
+
+  client.enterprise.addAppUser(
+    userName,
+    {
+      space_amount: spaceAmount
+    },
+    callback
+  );
+
+  function callback(err, res) {
+    if (err) throw err;
+    // console.log(res)
+  }
+}
 app.listen(PORT, () => {
-    console.log("App running on port " + PORT + "!");
-  })
-  
+  console.log("App running on port " + PORT + "!");
+})
